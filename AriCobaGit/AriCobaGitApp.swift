111d497ae5784be56eca6e032f084cde48802d21
Ari Kurniawan <ari.kurniawan@ciputra.ac.id>
2023-06-21 09:17:17 +0700

    Initial Commit

---
+++ AriCobaGit/AriCobaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AriCobaGitApp.swift
+//  AriCobaGit
+//
+//  Created by Ari Kurniawan on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AriCobaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
